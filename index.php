694041d12a6eaa6fb592a69df574d0684b544f2d
bitschua <167282380+bitschua@users.noreply.github.com>
2025-12-16 21:38:58 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 /**
- * User Dashboard Administration Screen
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
- * @subpackage Administration
- * @since 3.1.0
  */
 
-/** Load WordPress Administration Bootstrap */
-require_once __DIR__ . '/admin.php';
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-require ABSPATH . 'wp-admin/index.php';
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
